82fce799ee2507e0d5d590f0fea9f102db5259d3
Krishna Prasad Khanal <krishnaamen@gmail.com>
2023-02-14 16:33:18 +0100

    Initial Commit

---
+++ WordGame/WordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGameApp.swift
+//  WordGame
+//
+//  Created by min phone on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
